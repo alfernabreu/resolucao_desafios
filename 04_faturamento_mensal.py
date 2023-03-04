49e046b87b5e3516e5887e8de171a721db16a6ab
alfernab <86300297+alfernabreu@users.noreply.github.com>
2023-03-03 21:13:41 -0300

    04_faturamento_mensal.py final version

--- 04_faturamento_mensal.py
+++ 04_faturamento_mensal.py
@@ -1,6 +1,6 @@
 # 04_faturamento_mental.py
 
-# 4) Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+# Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
 
 # SP – R$67.836,43
 # RJ – R$36.678,66
@@ -21,10 +21,10 @@ total = estado_sp + estado_rj + estado_mg + estado_es + outros_estados
 print("\nBem-vindo(a)!")
 print(f"O faturamento mensal da distribuidora foi igual a:\nNo Estado do Espírito Santo: R${estado_es}\nNo Estado de Minas Gerais: R${estado_mg}\nNo Estado do Rio de Janeiro: R${estado_rj}\nNo Estado de São Paulo: R${estado_sp}\nNos demais estados: R${outros_estados}")
 print(f"O total do faturamento é de: R${total}")
+
 def calculo_percentual(x):
     resultado = (x / total) * 100
     return resultado
-
 
 def escolha_estado():
     repete = True
@@ -35,24 +35,19 @@ def escolha_estado():
 
         match estado:
             case 1:
-                print(
-                    f"\nO percentual de representação do Estado do Espírito Santo em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_es):.2f}%.")
+                print(f"\nO percentual de representação do Estado do Espírito Santo em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_es):.2f}%.")
                 print("\n===========Consulta concluída===========")
             case 2:
-                print(
-                    f"\nO percentual de representação do Estado de Minas Gerais em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_mg):.2f}%.")
+                print(f"\nO percentual de representação do Estado de Minas Gerais em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_mg):.2f}%.")
                 print("\n===========Consulta concluída===========")
             case 3:
-                print(
-                    f"\nO percentual de representação do Estado do Rio de Janeiro em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_rj):.2f}%.")
+                print(f"\nO percentual de representação do Estado do Rio de Janeiro em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_rj):.2f}%.")
                 print("\n===========Consulta concluída===========")
             case 4:
-                print(
-                    f"\nO percentual de representação do Estado de São Paulo em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_sp):.2f}%.")
+                print(f"\nO percentual de representação do Estado de São Paulo em relação ao faturamento total da distribuidora é de {calculo_percentual(estado_sp):.2f}%.")
                 print("\n===========Consulta concluída===========")
             case 5:
-                print(
-                    f"\nO percentual de representação dos demais estados da federação em relação ao faturamento total da distribuidora é de {calculo_percentual(outros_estados):.2f}%")
+                print(f"\nO percentual de representação dos demais estados da federação em relação ao faturamento total da distribuidora é de {calculo_percentual(outros_estados):.2f}%")
                 print("\n===========Consulta concluída===========")
             case _:
                 print("\nPor favor, digite um número válido.")
@@ -63,9 +58,10 @@ def escolha_estado():
             repete = input('\nATENÇÃO! Você deve digitar "Sim" ou "Não"\n').lower()
 
         if repete == "não":
-            print("Obrigado pela preferência.")
             repete = False
+            print("Obrigado pela preferência.")
         else:
             repete = True
+
 
 escolha_estado()
